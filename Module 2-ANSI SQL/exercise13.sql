0602d3dbb334baa05ade0911e29ae26e9aea55fc
SWATHI-RAJAKUMAR <swathirajakumar2005@gmail.com>
2025-05-26 22:45:09 +0530

    Add files via upload

---
+++ Module 2-ANSI SQL/exercise13.sql
@@ -0,0 +1,9 @@
+SELECT 
+    e.city,
+    AVG(f.rating) AS average_rating
+FROM 
+    Feedback f
+JOIN 
+    Events e ON f.event_id = e.event_id
+GROUP BY 
+    e.city;
---
+++ Module 2-ANSI SQL/exercise4.sql
@@ -0,0 +1,11 @@
+SELECT 
+    e.title,
+    COUNT(s.session_id) AS peak_sessions
+FROM 
+    Sessions s
+JOIN 
+    Events e ON s.event_id = e.event_id
+WHERE 
+    TIME(s.start_time) BETWEEN '10:00:00' AND '12:00:00'
+GROUP BY 
+    e.event_id;
